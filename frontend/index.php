8cb5c3f3e212a23ce21539cf45bebb2549c3eb3f
lucaFiorini <luca.fio45@gmail.com>
2023-11-24 12:07:45 +0100

    Working on it blud

---
+++ frontend/index.php
@@ -0,0 +1,26 @@
+
+<?require "basic_start.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <?require "header.php"?>
+        <main>
+            Prodotti:
+            <div class="table" id="products-table">
+                <?for($i = 0; true;) print("hello")?>
+                    <div class="col">
+                        <div class="row">
+                            
+                        </div>
+                    </div>
+                <?endfor;?>
+            </div>
+        </main>
+        <?require "footer.php"?>
+    </body>
+</html>
